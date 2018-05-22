db5d94a04ae7be05dadea36b43695fac319ef678
twitte <tijmenwitte@gmail.com>
2018-05-22 10:13:38 +0200

    Calibration values global

--- in4073/in4073.h
+++ in4073/in4073.h
@@ -80,6 +80,7 @@ bool i2c_read(uint8_t slave_addr, uint8_t reg_addr, uint8_t length, uint8_t *dat
 // MPU wrapper
 int16_t phi, theta, psi;
 int16_t sp, sq, sr;
+int16_t cal_sr, cal_phi, cal_theta, cal_psi, cal_sp, cal_sq, cal_sax, cal_say, cal_saz;
 int16_t sax, say, saz;
 uint8_t sensor_fifo_count;
 void imu_init(bool dmp, uint16_t interrupt_frequency); // if dmp is true, the interrupt frequency is 100Hz - otherwise 32Hz-8kHz
